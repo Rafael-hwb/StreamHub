044955ef2ba4e3b0f0825ed5fc85c1bd82813f59
huang-weiboRafeal <a2357741727@163.com>
2026-07-23 14:46:58 +0800

    build stream handler

--- streamsever/defs.go
+++ streamsever/defs.go
@@ -4,6 +4,11 @@ import (
 	"net/http"
 )
 
+const(
+	VIDEO_DIR = "./videos"
+	MAX_UPLOAD_SIZE = 1024*1024*50 //50MB
+)
+
 type ErrStruct struct {
 	ErrMessage string `json:"error_message"`
 	ErrCode    string `json:"error_code"`
@@ -26,6 +31,14 @@ var (
 		HttpCode: http.StatusInternalServerError,
 		Err: ErrStruct{
 			ErrMessage: "Internal server error.",
+			ErrCode:    "002",
+		},
+	}
+
+		ErrorFileTooBig = ErrResponse{
+		HttpCode: http.StatusBadRequest,
+		Err: ErrStruct{
+			ErrMessage: "File is too big.",
 			ErrCode:    "002",
 		},
 	}
--- streamsever/handlers.go
+++ streamsever/handlers.go
@@ -1,11 +1,49 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+	"net/http"
+	"time"
+	"github.com/gin-gonic/gin"
+)
 
 func StreamHandler(context *gin.Context) {
+	vid := context.Param("vid-id")
+	videoLink := VIDEO_DIR + vid
 
+	video, err := os.Open(videoLink)
+	if err != nil{
+		SendErrorResponse(context, ErrorInternalFaults)
+		return
+	}
+	context.Header("Content-Type", "video/mp4")
+	http.ServeContent(context.Writer, context.Request, "", time.Now(), video)
+
+	defer video.Close()
 }
 
 func UploadHandler(context *gin.Context) {
+	err := context.Request.ParseMultipartForm(MAX_UPLOAD_SIZE)
+	if err != nil {
+		SendErrorResponse(context, ErrorFileTooBig)
+		return
+	}
 
+	vid := context.Param("vid-id")
+
+	file, err := context.FormFile("file")
+	if err != nil {
+		SendErrorResponse(context, ErrorInternalFaults)
+		return
+	}
+
+	err = context.SaveUploadedFile(file, VIDEO_DIR+vid)
+	if err != nil {
+		SendErrorResponse(context, ErrorInternalFaults)
+		return
+	}
+
+	SendNormalResponse(context, http.StatusCreated, gin.H{
+		"success": true,
+	})
 }
